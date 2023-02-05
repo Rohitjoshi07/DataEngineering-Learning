29279c8145256a4137ce140d0f0298967dc8c1cd
Rohit <rohitjoshi9july@gmail.com>
2023-02-05 11:52:51 +0000

    updated github deployment file

--- week_2_workflow_orchestration/homework/github_deploy.py
+++ week_2_workflow_orchestration/homework/github_deploy.py
@@ -1,16 +1,14 @@
 from prefect.filesystems import GitHub
-from etl_we_2_gcs import etl_web_2_gcs
+from etl_web_to_gcs import etl_web_2_gcs
 from prefect.deployments import Deployment
 
 github_block = GitHub.load("github-store")
 
-deploy_blcok = Deployment.build(
+deploy_block = Deployment.build_from_flow(
     flow= etl_web_2_gcs,
-    name= "github ETL flow",
-    infrastructure= github_block
+    name= "github-ETL-flow",
+    storage=github_block
 )
 
 if __name__=="__main__":
     deploy_block.apply()
-
-    
